bc92bef1ac87fbb3955c3f6fc717ba9956477e78
Ronie <roniemeque@icloud.com>
2020-06-15 17:36:58 -0300

    adding resources for my Next.js class on Fatec

---
+++ src/pages/curso-next.tsx
@@ -0,0 +1,57 @@
+const NextCourseUsefulLinksPage = () => {
+  return (
+    <main>
+      <h1>
+        Curso Next.js
+      </h1>
+
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://gitpod.io/#https://github.com/roniemeque/teaching-nextjs"
+          >
+            Atalho GitPod
+          </a>
+        </li>
+
+        <li>
+          <a
+            target="_blank"
+            href="https://github.com/roniemeque/teaching-nextjs"
+          >
+            Código inicial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://github.com/roniemeque/teaching-nextjs/tree/final-ssr"
+          >
+            Código final (SSR)
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://github.com/roniemeque/teaching-nextjs/tree/final-ssg"
+          >
+            Código final (SSG)
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://final-ssg.now.sh/">
+            Aplicação rodando (SSG)
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://nextjs.org/docs/getting-started">
+            Documentação Next.js
+          </a>
+        </li>
+      </ul>
+    </main>
+  );
+};
+
+export default NextCourseUsefulLinksPage;
